6300f4f0708ee032ad0184e68c0961b51e6e20c8
Jamgoori <lks2354@naver.com>
2023-06-27 00:22:20 +0900

    오류복원

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; 
-import { getFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
   apiKey: "AIzaSyAF7LLxHKh23ratBnTMQQ6nql9egXExJ6M",
   authDomain: "jampoll.firebaseapp.com",
@@ -17,6 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-const db = getFirestore(app);
 
-export default {auth , db};
+export default auth;
