d5a54ee969e93db179b7ebfe2c31fd5ca9866244
Jamgoori <lks2354@naver.com>
2023-06-28 21:09:56 +0900

    정렬순서

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,6 +1,14 @@
-import { useState, useEffect } from 'react';
-import { db } from '../firebase';
-import { collection, getDocs, updateDoc, doc, deleteDoc } from 'firebase/firestore';
+import { useState, useEffect } from "react";
+import { db } from "../firebase";
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  updateDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 
 const PostList = () => {
   const [title, setTitle] = useState([]);
@@ -11,35 +19,56 @@ const PostList = () => {
     setTitle(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
   };
 
-
-  // 글수정하기
   const updateTitle = async (id, title) => {
     const titleDoc = doc(db, "board", id);
     const newFields = { title: title + "변경됨" };
     await updateDoc(titleDoc, newFields);
-    getTitle(); // 업데이트 후 데이터 갱신
+    getTitle();
   };
 
-  const deleteTitle = async(id) => {
+  const deleteTitle = async (id) => {
     const titleDoc = doc(db, "board", id);
     await deleteDoc(titleDoc);
-    getTitle(); // 업데이트 후 데이터 갱신
-  }
+    getTitle();
+  };
 
   useEffect(() => {
-    getTitle(); // 초기 데이터 가져오기
+    const fetchData = async () => {
+      const q = query(collection(db, "board"), orderBy("title", "desc"));
+      const querySnapshot = await getDocs(q);
+      setTitle(
+        querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      );
+    };
+
+    fetchData();
   }, []);
 
   return (
-    <div>
+    <div className="ddd">
       {title.map((title) => {
         return (
-          <>
+          <div key={title.id}>
             <div>제목: {title.title}</div>
-            <div>토의: {title.dscuss1}</div>
-            <button onClick={() => { updateTitle(title.id, title.title) }}>제목 변경하기</button>
-            <button onClick={() => {deleteTitle(title.id)}}>글 삭제하기</button>
-          </>
+            <div>토의: {title.discuss1}</div>
+            <div className="flex">
+              <button>글수정하기</button>
+              <button
+                onClick={() => {
+                  updateTitle(title.id, title.title);
+                }}
+              >
+                제목 변경하기
+              </button>
+            </div>
+            <button
+              onClick={() => {
+                deleteTitle(title.id);
+              }}
+            >
+              글 삭제하기
+            </button>
+          </div>
         );
       })}
     </div>
