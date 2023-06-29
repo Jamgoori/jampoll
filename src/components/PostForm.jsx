6fc22ee57539b92f6d4c6f7220cfa5663279bdd4
Jamgoori <lks2354@naver.com>
2023-06-29 21:39:30 +0900

    서브 컬렉션 + 서브컬렉션 문서의 필드 추가

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -11,20 +11,44 @@ import {
 const PostForm = () => {
   const [newTitle, setNewTitle] = useState("");
   const [newSubcollection, setNewSubcollection] = useState("");
+  const [newSubcollectionId, setNewSubcollectionId] = useState("");
+  const [fieldList, setFieldList] = useState([{ fieldName: "" }]);
+
+  const handleFieldChange = (index, field, value) => {
+    const updatedFieldList = [...fieldList];
+    updatedFieldList[index] = { ...updatedFieldList[index], [field]: value };
+    setFieldList(updatedFieldList);
+  };
+
+  const handleAddField = () => {
+    setFieldList([...fieldList, { fieldName: "" }]);
+  };
 
   const createTitle = async () => {
     const newPost = {
       title: newTitle,
       createdat: serverTimestamp(),
-      subcollection: newSubcollection,
     };
 
     const boardCollectionRef = collection(db, "board");
     const newTitleDocRef = doc(boardCollectionRef);
 
-    await setDoc(newTitleDocRef, newPost);
+    const newTitleDoc = await setDoc(newTitleDocRef, newPost);
+
+    const subcollectionRef = collection(newTitleDocRef, newSubcollectionId);
+    const subcollectionDocRef = doc(subcollectionRef);
+
+    const subcollectionData = {};
+    fieldList.forEach(({ fieldName }, index) => {
+      subcollectionData[fieldName] = index;
+    });
+
+    await setDoc(subcollectionDocRef, subcollectionData);
+
     setNewTitle("");
     setNewSubcollection("");
+    setNewSubcollectionId("");
+    setFieldList([{ fieldName: "" }]);
   };
 
   return (
@@ -37,14 +61,26 @@ const PostForm = () => {
           setNewTitle(e.target.value);
         }}
       />
+
       <input
         className="width100"
-        placeholder="서브컬렉션"
-        value={newSubcollection}
+        placeholder="서브컬렉션 ID"
+        value={newSubcollectionId}
         onChange={(e) => {
-          setNewSubcollection(e.target.value);
+          setNewSubcollectionId(e.target.value);
         }}
       />
+      {fieldList.map((field, index) => (
+        <div key={index}>
+          <input
+            className="width100"
+            placeholder="필드명"
+            value={field.fieldName}
+            onChange={(e) => handleFieldChange(index, "fieldName", e.target.value)}
+          />
+        </div>
+      ))}
+      <button onClick={handleAddField}>필드 추가</button>
       <button onClick={createTitle}>글 쓰기</button>
     </div>
   );
